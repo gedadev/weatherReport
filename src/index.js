1c61f816f26d3859d669381811d770811678d3ed
gedadev <gjbg1234@gmail.com>
2023-03-02 23:02:33 -0600

    added the function to get the coordinates of the city entered by the user, whichcall another function that gets the weather information of the city

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('a')
+async function currentWeather(lat, lon) {
+    console.log(lat, lon);
+    const resp = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5d0907efdadf22c907135983ee95c9d4`, {mode: 'cors'});
+    const data = await resp.json();
+    console.log(data);
+}
+
+async function getCoordinates(city) {
+    const resp = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=5d0907efdadf22c907135983ee95c9d4`, {mode: 'cors'});
+    const data = await resp.json();
+    currentWeather(data[0].lat, data[0].lon);
+}
+
+getCoordinates('mexico');
