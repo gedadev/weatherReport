945b66fd2e823db1ede73141ab8e50674b6046ab
gedadev <gjbg1234@gmail.com>
2023-03-03 21:12:07 -0600

    refactored the index.js file to bundle the funtions in a different file, added a listener to call the class with the city entered by the user

---
+++ src/app/getForecast.js
@@ -0,0 +1,14 @@
+export class Forecast {
+
+    async currentWeather(lat, lon) {
+        const resp = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5d0907efdadf22c907135983ee95c9d4`, {mode: 'cors'});
+        const data = await resp.json();
+        console.log(data);
+    }
+    
+    async getCoordinates(city) {
+        const resp = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=5d0907efdadf22c907135983ee95c9d4`, {mode: 'cors'});
+        const data = await resp.json();
+        this.currentWeather(data[0].lat, data[0].lon);
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import './style.css';
+import { Forecast } from './app/getForecast';
 
-async function currentWeather(lat, lon) {
-    console.log(lat, lon);
-    const resp = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5d0907efdadf22c907135983ee95c9d4`, {mode: 'cors'});
-    const data = await resp.json();
-    console.log(data);
-}
+const searchInput = document.querySelector('#city-input');
+const searchBtn = document.querySelector('#city-btn');
 
-async function getCoordinates(city) {
-    const resp = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=5d0907efdadf22c907135983ee95c9d4`, {mode: 'cors'});
-    const data = await resp.json();
-    currentWeather(data[0].lat, data[0].lon);
-}
+let forecast = new Forecast();
 
-getCoordinates('mexico');
+searchBtn.addEventListener('click', () => {
+    forecast.getCoordinates(searchInput.value);
+});
