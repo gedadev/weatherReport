d121549e58ed831bc25c7ae33a68b4f994d74805
gedadev <gjbg1234@gmail.com>
2023-03-23 20:47:03 -0600

    fixed the toggle units button to make it work with the geolocation function

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const searchBtn = document.querySelector('#city-btn');
 const container = document.querySelector('#forecast-container');
 const units = document.querySelector('#units-selector');
 
+searchBtn.addEventListener('click', getForecast);
+
+currentPosition();
+
 units.addEventListener('click', () => {
     if (units.checked) {
         units.parentNode.children[1].style.color = 'var(--color-1)';
@@ -14,14 +18,19 @@ units.addEventListener('click', () => {
         units.parentNode.children[1].style.color = 'var(--color-4)';
         units.parentNode.children[3].style.color = 'var(--color-1)';
     }
+
+    searchInput.value === "" ? currentPosition() : getForecast();
+});
+
+function getForecast() {
     let forecast = new Forecast(searchInput.value, container, (units.checked ? 'imperial' : 'metric'));
     forecast.run();
-});
-searchBtn.addEventListener('click', () => {
-    let forecast = new Forecast(searchInput.value, container, (units.checked ? 'imperial' : 'metric'));
-    forecast.run();
-});
-  
+}
+
+function currentPosition() {
+    navigator.geolocation.getCurrentPosition(success, error, {timeout: 3000});
+}
+
 function success(pos) {
     const crd = pos.coords;
     let forecast = new Forecast(searchInput.value, container, (units.checked ? 'imperial' : 'metric'), crd.latitude, crd.longitude);
@@ -30,6 +39,5 @@ function success(pos) {
 
 function error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`);
+    // Add a msg to indicate the user to turn on the browser location or to enter a value in the search box
 }
-
-navigator.geolocation.getCurrentPosition(success, error, {timeout: 3000});
